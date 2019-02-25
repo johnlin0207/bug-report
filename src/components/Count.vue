bd2a14af80e8803bd2bd2af3b620f6e975a5d177
Johlin <johnlin0207@163.com>
2019-02-25 18:03:46 +0800

    new files

---
+++ src/components/Count.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Button @click="$emit('my-event')">+</Button>
+    <h1>{{this.num}}</h1>
+    <el-button type="primary">主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "count",
+    props: {
+      num: Number
+    },
+    method: {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <Header/>
+  </div>
+</template>
+
+<script>
+  // @ is an alias to /src
+  import Header from '@/components/home/Header.vue';
+
+  export default {
+    name: 'home',
+    data() {
+      return {
+        num: 0
+      }
+    },
+    components: {
+      Header,
+    },
+    methods: {
+      add() {
+        this.num = this.num + 1
+      }
+  }
+}
+</script>
